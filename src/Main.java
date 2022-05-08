11b85c65415ee3893338aa2090f8052413285c72
Kari Turtiainen <kp.turtiainen7@gmail.com>
2022-05-08 20:01:39 +0300

    added jar and updated readme

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Snake game
+ * Developed for course "TKO8971 Soveltava projekti"
+ * @author kptur - Kari-Pekka Turtiainen
+ */
 
 public class Main{
     public static void main(String[] args){
